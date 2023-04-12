9afb39b98122e4ad2fa3373f05a58085d542e9cc
anras5 <anras1filip@gmail.com>
2023-04-12 19:06:19 +0200

    Update README.md

---
+++ README.md
@@ -0,0 +1,20 @@
+# todo-app-backend
+
+## How to run the app
+```commandline
+docker compose up --build
+```
+
+## Description
+Simple REST backend application written Go.
+Available endpoints:
+- `GET /todos`
+- `GET /todos/:id`
+- `POST /todos`
+- `PUT /todos/:id`
+- `DELETE /todos/:id`
+- `PUT /todos/:id/complete`
+- `PUT /todos/:id/incomplete`
+- `GET /todos?completed=true`
+- `GET /todos?completed=false`
+
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -27,7 +27,7 @@ func main() {
 	}
 
 	// -------------------------------------------------------------------------------------------- //
-	//Set up loggers
+	// Set up loggers
 	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	app.InfoLog = infoLog
 	errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
@@ -35,7 +35,7 @@ func main() {
 
 	// -------------------------------------------------------------------------------------------- //
 	// Connect to database
-	log.Println("Connecting to database")
+	app.InfoLog.Println("Connecting to database")
 	db, err := driver.ConnectSQL(fmt.Sprintf("host=postgres-db port=5432 dbname=todos user=%s password=%s sslmode=%s",
 		os.Getenv("DB_USER"), os.Getenv("DB_PASSWD"), os.Getenv("SSL_MODE")))
 	if err != nil {
